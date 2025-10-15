8864baabb0f874e8a85137343ea2162c2230e31b
Saeed Nemati <sn.1361@gmail.com>
2025-10-15 09:22:42 -0400

    Getting the BIOS info

---
+++ commands/pcInfo/bios.js
@@ -0,0 +1,18 @@
+import { execSync } from 'child_process'
+import { info, check, error } from '../../logger.js'
+
+function run(cmd) {
+    try {
+        return execSync(cmd).toString().trim()
+    } catch {
+        return ''
+    }
+}
+
+export default () => {
+    const biosDate = run("sudo dmidecode -t bios | grep 'Release Date' | awk '{print $3}'") || 'Unknown'
+    if (biosDate !== 'Unknown')
+        check(`BIOS Date: ${biosDate}`)
+    else
+        error('BIOS Date could not be determined ❌')
+}
