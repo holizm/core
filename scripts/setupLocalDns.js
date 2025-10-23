6ecef760a56cedc083f0322e71fd6e75b09c3e8e
Saeed Nemati <sn.1361@gmail.com>
2025-10-23 14:04:42 -0400

    setting the local DNS

---
+++ scripts/setupLocalDns.js
@@ -0,0 +1,11 @@
+import fs from 'fs'
+import { getFileContent } from "./os.js"
+
+export default params => {
+    const { host } = params
+    let content = getFileContent('/etc/hosts')
+    if (!content.includes(` ${host}`) && !content.includes(`\t${host}`)) {
+        if (!content.endsWith('\n')) content += '\n'
+        fs.appendFileSync('/etc/hosts', `127.0.0.1 ${host}\n`)
+    }
+}
