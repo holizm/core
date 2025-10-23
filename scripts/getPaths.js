12a9c217113e7039a2f572f0259d38b51ba87501
Saeed Nemati <sn.1361@gmail.com>
2025-10-23 08:52:23 -0400

    fixing the relative path, not working in root

--- scripts/getPaths.js
+++ scripts/getPaths.js
@@ -3,17 +3,17 @@ export default ({
     process,
 }) => {
     const paths = {
-        commonPath: `/${repo}/common`,
-        connectionStringsPath: `/${repo}/common/connectionStrings.json`,
-        dependenciesPath: `/${repo}/common/dependencies`,
-        initialPath: `/${repo}/common/initial.sql`,
-        menusDirectoryPath: `/${repo}/${process}/menus`,
-        migrationPath: `/tmp/${repo}/migration`,
-        privateSettingsPath: `/${repo}/common/privateSettings.json`,
-        processPath: `/${repo}/${process}`,
-        publicSettingsPath: `/${repo}/common/publicSettings.json`,
-        settingsOverridePath: `/${repo}/${process}/settingsOverride.json`,
-        tenantsPath: `/${repo}/common/tenants`,
+        commonPath: `~/${repo}/common`,
+        connectionStringsPath: `~/${repo}/common/connectionStrings.json`,
+        dependenciesPath: `~/${repo}/common/dependencies`,
+        initialPath: `~/${repo}/common/initial.sql`,
+        menusDirectoryPath: `~/${repo}/${process}/menus`,
+        migrationPath: `~/tmp/${repo}/migration`,
+        privateSettingsPath: `~/${repo}/common/privateSettings.json`,
+        processPath: `~/${repo}/${process}`,
+        publicSettingsPath: `~/${repo}/common/publicSettings.json`,
+        settingsOverridePath: `~/${repo}/${process}/settingsOverride.json`,
+        tenantsPath: `~/${repo}/common/tenants`,
     }
     return paths
 }
