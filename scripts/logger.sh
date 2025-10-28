a88da254edb1f341a2d207f5d9b3d7caed955419
Saeed Nemati <sn.1361@gmail.com>
2025-10-28 12:34:00 -0400

    adding the stop command to forcefully stop local containers

--- scripts/logger.sh
+++ scripts/logger.sh
@@ -15,3 +15,6 @@ info() { printf "${color_cyan}%s${color_reset}\n" "$*"; }
 warning() { printf "${color_yellow}%s${color_reset}\n" "$*"; }
 error() { printf "${color_red}%s${color_reset}\n" "$*" >&2; }
 divide() { printf "\n${color_magenta}----------${color_reset}\n\n"; }
+check() {
+    printf "${color_green}%s;%s${color_reset}\n" "$*" "$check_mark" | column -t -s ';'
+}
