ce4d4af5b51fd5821c2e81fa4d8fa7a02241131e
Saeed Nemati <sn.1361@gmail.com>
2025-10-29 08:52:08 -0400

    committed by script

---
+++ scripts/status.js
@@ -0,0 +1,32 @@
+import { runOnTerminal } from "./terminal.js"
+import findGits from './findGits.js'
+import {
+    divide,
+    info,
+    warning,
+} from './logger.js'
+
+function printStatus(repoPath) {
+    try {
+        const status = runOnTerminal(`git -C ${repoPath} status`)
+        if (/Changes|Untracked/.test(status)) {
+            info(repoPath)
+            const porcelain = runOnTerminal(`git -C ${repoPath} status --porcelain`)
+            console.log(porcelain.trim())
+            divide()
+        } else if (/ahead/.test(status)) {
+            warning(`Push ${repoPath}`)
+            divide()
+        } else if (/diverged/.test(status)) {
+            warning(`Sync ${repoPath}`)
+            divide()
+        }
+    } catch (err) {
+        console.error(`\x1b[31mError checking ${repoPath}: ${err.message}\x1b[0m`)
+    }
+}
+
+export default search => {
+    const gitDirs = findGits(search)
+    for (const repo of gitDirs) printStatus(repo)
+}
