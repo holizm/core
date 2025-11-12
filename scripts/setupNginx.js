11b1b8ca02c01db6634a133559ef5ccfdaafd27b
Saeed Nemati <sn.1361@gmail.com>
2025-11-12 09:07:05 -0500

    committed by script

--- scripts/setupNginx.js
+++ scripts/setupNginx.js
@@ -1,28 +1,11 @@
-import {
-    getFileContent,
-    writeFile,
-} from "./os.js"
+import { replaceVariables } from "./os.js"
 import { runOnTerminal } from "./terminal.js"
 
 export default params => {
     const {
         home,
         host,
-        tenant,
-        repo,
-        process,
     } = params
-
-    params.nginxParamsServerName = "$server_name"
-    params.nginxParamsRequestUri = "$request_uri"
-    params.nginxHostParameter = "$host"
-    params.nginxSchemeParameter = "$scheme"
-    params.nginxForParameter = "$proxy_add_x_forwarded_for"
-    params.nginxHttpUpgradeParameter = "$http_upgrade"
-    params.nginxParamsSubdomain = "$subdomain"
-    params.multitenant = "$multitenant"
-    params.one = "$1"
-    params.two = "$2"
 
     const confDir = `${home}/nginx/conf.d`
     const includesDir = `${home}/nginx/includes`
@@ -31,16 +14,8 @@ export default params => {
     const confFile = `${confDir}/${host}.conf`
     const includesFile = `${includesDir}/${host}`
 
-    const includesTemplate = getFileContent(includesTemplatePath)
-    const confTemplateRaw = getFileContent(templatePath)
-
-    const includesContent = includesTemplate.replace(/\$(\w+)/g, (_, name) => params[name] || "")
-
-    let confTemplate = confTemplateRaw.replace("$host", host).replace("$tenant", tenant)
-    const confContent = confTemplate.replace(/\$(\w+)/g, (_, name) => params[name] || "")
-
-    writeFile(includesFile, includesContent)
-    writeFile(confFile, confContent)
+    replaceVariables(templatePath, confFile, params)
+    replaceVariables(includesTemplatePath, includesFile, params)
 
     runOnTerminal("sudo nginx -t && sudo systemctl reload nginx 1>/dev/null 2>&1")
 }
