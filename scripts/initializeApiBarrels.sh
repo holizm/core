cef3e6ea0c9c02c74172c55eb1613324304de54b
Saeed Nemati <sn.1361@gmail.com>
2026-01-05 11:30:37 +0330

    committed by script

--- scripts/initializeApiBarrels.sh
+++ scripts/initializeApiBarrels.sh
@@ -1,3 +1,3 @@
-export directories=$(find /spl -maxdepth 1 -mindepth 1 -type d | sed 's|/spl/||' | sort)
-node /core/commands/api/generateExports.js $directories
-node /core/commands/api/generatePackageJsonFiles.js $directories
+export directories=$(find $home/spl -maxdepth 1 -mindepth 1 -type d | sed 's|/spl/||' | sort)
+node $home/core/commands/api/generateExports.js $directories
+node $home/core/commands/api/generatePackageJsonFiles.js $directories
