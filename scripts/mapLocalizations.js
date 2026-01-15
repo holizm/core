bc6bc0fe5e6a1a1d7408c0bd63e851c4742377ad
Saeed Nemati <sn.1361@gmail.com>
2026-01-15 14:51:04 +0330

    committed by script

--- scripts/mapLocalizations.js
+++ scripts/mapLocalizations.js
@@ -5,6 +5,15 @@ export default params => {
     const {
         home,
     } = params
+    const coreLocalizations = [
+        `${home}/core/localization`,
+        `${home}/api/localization`,
+        `${home}/panel/localization`,
+        `${home}/site/localization`,
+    ]
+    for (const item of coreLocalizations) {
+        params.addVolume(item, item)
+    }
     const findCommand = `
         find ${home} -type d -name '.git' 2>/dev/null | 
         while read gitdir; do
@@ -16,9 +25,6 @@ export default params => {
     const dependencies = getDependencies(params)
     const items = runOnTerminal(findCommand).split('\n')
     for (const item of items) {
-        if (item.includes('/core/')) {
-            params.addVolume(item, item)
-        }
         if (dependencies.some(dependency => item.includes(`/${dependency}/`))) {
             params.addVolume(item, item)
         }
