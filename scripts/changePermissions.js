493f90a897f149a09485ab65f8ddaed42801c6be
Saeed Nemati <sn.1361@gmail.com>
2026-02-24 08:44:41 +0330

    committed by script

---
+++ scripts/changePermissions.js
@@ -0,0 +1,21 @@
+import { runOnTerminal } from './terminal.js'
+
+export default params => {
+    const {
+        repo,
+        processType,
+    } = params
+    const dirs = [
+        `/tmp/${repo}`,
+        `/var/tmp/${processType}`,
+        `/var/tmp/${repo}`,
+    ]
+
+    dirs.forEach(dir => {
+        runOnTerminal(`
+            if [ -d "${dir}" ]; then
+                find "${dir}" -user root -exec chown -h dev:dev {} +
+            fi
+        `)
+    })
+}
