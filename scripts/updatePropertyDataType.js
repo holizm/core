9f4801da46f62be36f1682759d559677d910d674
Saeed Nemati <sn.1361@gmail.com>
2026-03-11 17:05:32 +0330

    committed by script

---
+++ scripts/updatePropertyDataType.js
@@ -0,0 +1,32 @@
+var dbName = "database";
+var collectionName = "collection";
+var fieldName = "property";
+var sourceType = "string";
+var targetType = "int";
+
+var collection = db[collectionName];
+var totalDocs = collection.countDocuments({ [fieldName]: { $exists: true } });
+
+print("Updating " + totalDocs + " documents in " + dbName + "." + collectionName);
+print("Field: " + fieldName + " (" + sourceType + " -> " + targetType + ")");
+
+var result = collection.updateMany(
+    { [fieldName]: { $exists: true } },
+    [
+        {
+            $set: {
+                [fieldName]: {
+                    $convert: {
+                        input: "$" + fieldName,
+                        to: "int",
+                        onError: "$" + fieldName,
+                        onNull: null
+                    }
+                }
+            }
+        }
+    ]
+);
+
+print("Modified: " + result.modifiedCount);
+print("Matched: " + result.matchedCount);
