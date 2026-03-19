5cc33061ffcea9c683f08cc03e4e947393bc40f3
Saeed Nemati <sn.1361@gmail.com>
2026-03-19 09:21:36 +0330

    committed by script

--- scripts/findNonPartRepos.js
+++ scripts/findNonPartRepos.js
@@ -1,5 +1,8 @@
-import { getContent } from './os.js'
-import { runOnTerminal } from "./terminal.js"
+import {
+    getContent,
+    isFile,
+} from './os.js'
+import { runOnTerminal } from './terminal.js'
 
 const home = process.env.HOME
 const cacheFile = '/tmp/nonPartRepos.json'
@@ -12,14 +15,14 @@ export default () => {
         find ${home} -maxdepth 2 -type d -name .git | cut -d'/' -f4 | sort |
         while read repo;
         do
-            if [ ! -f ${home}/${repo}/part ]; then
-                if [ ! -d ${home}/${repo}/common ]; then
+            if [ ! -f ${home}/$repo/part ]; then
+                if [ ! -d ${home}/$repo/common ]; then
                     echo $repo
                 fi
             fi
         done
     `
     const output = runOnTerminal(command)
-    const repos = output.split('\n').map(i => i.trim()).filter(i)
+    const repos = output.split('\n').map(i => i.trim()).filter(i => i)
     return repos
 }
