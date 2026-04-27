4faefec7a1e23ff67d7316d20af3b44bc47ffdaf
Saeed Nemati <sn.1361@gmail.com>
2026-04-27 05:50:44 -0400

    committed by script

--- scripts/pcInfo/bios.js
+++ scripts/pcInfo/bios.js
@@ -2,7 +2,7 @@ import { runOnTerminal } from '../../scripts/terminal.js'
 import { info, check, error } from '../../scripts/logger.js'
 
 export default () => {
-    const biosDate = runOnTerminal('sudo dmidecode -t bios | grep 'Release Date' | awk '{ print $3 }'') || 'Unknown'
+    const biosDate = runOnTerminal(`sudo dmidecode -t bios | grep 'Release Date' | awk '{print $3}'`) || 'Unknown'
     if (biosDate !== 'Unknown')
         check(`BIOS Date: ${biosDate}`)
     else
