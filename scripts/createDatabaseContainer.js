b838b6e0a2bdfd64917715a3b79d3d8ff73eff01
Saeed Nemati <sn.1361@gmail.com>
2026-06-03 17:03:09 +0330

    committed by script

--- scripts/createDatabaseContainer.js
+++ scripts/createDatabaseContainer.js
@@ -50,10 +50,6 @@ export default params => {
         ...params,
         host: `${repo}.local`,
     })
-    setupLocalDns({
-        ...params,
-        host: `${repo}.prod`,
-    })
     params.databaseEnginePort = getDeterministicPort(repo)
     const lines = getLines(tenantsPath, 'utf8').filter(Boolean)
     lines.forEach(line => processTenantLine({
