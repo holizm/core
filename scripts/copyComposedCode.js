755f9e2873a6a66686e75c960143fc0b7362f3a3
Saeed Nemati <sn.1361@gmail.com>
2026-06-07 08:13:22 +0330

    committed by script

--- scripts/copyComposedCode.js
+++ scripts/copyComposedCode.js
@@ -8,7 +8,9 @@ export default params => {
         containerName,
         repo,
     } = params
+
     removeAndRecreateDir('/tmp/build')
+
     const containerCommand = `
         for dir in ${containerHome}/*; do
             if [ -d $dir ]; then
@@ -16,20 +18,51 @@ export default params => {
             fi
         done | sort
     `
+
     const hostCommand = `docker exec ${containerName} bash -c '${containerCommand}'`
     const dirs = runOnTerminal(hostCommand, { splitLines: true })
+
     for (const dir of dirs) {
         const name = getFileNameWithoutExtension(dir)
+
         if (name === 'packages') {
             continue
         }
+
         info(name)
+
         runOnTerminal(`
-        docker exec ${containerName} bash -c '
-            cd '${containerHome}' &&
-            tar --exclude='node_modules' -cf - '${name}'
-        ' | tar -xf - -C /tmp/build
-    `)
+            docker exec ${containerName} bash -c '
+                cd "${containerHome}" &&
+                tar --exclude="node_modules" -cf - "${name}"
+            ' | tar -xf - -C /tmp/build
+        `)
     }
-    // runOnTerminal(`docker cp ${containerName}:${containerHome}/spl /tmp/build/spl || true`)
+
+    const nodeModulesPath = `${containerHome}/node_modules`
+
+    const findPartModulesCommand = `
+        for dir in ${nodeModulesPath}/*; do
+            if [ -d "$dir" ]; then
+                if [ -f "$dir/part" ]; then
+                    echo "$dir"
+                fi
+            fi
+        done
+    `
+
+    const partModulesHostCommand = `docker exec ${containerName} bash -c '${findPartModulesCommand}'`
+    const partDirs = runOnTerminal(partModulesHostCommand, { splitLines: true })
+
+    for (const dir of partDirs) {
+        const name = getFileNameWithoutExtension(dir)
+        info(name)
+
+        runOnTerminal(`
+            docker exec ${containerName} bash -c '
+                cd "${nodeModulesPath}" &&
+                tar -cf - "${name}"
+            ' | tar -xf - -C /tmp/build/node_modules
+        `)
+    }
 }
