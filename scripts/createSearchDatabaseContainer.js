2b3e0587fdd5cd617b8069d3cad51d0839595810
MohammadMiras <mohammad.miras.1377@gmail.com>
2026-08-10 16:50:38 +0330

    committed by script

---
+++ scripts/createSearchDatabaseContainer.js
@@ -0,0 +1,64 @@
+import { runOnTerminal } from './terminal.js'
+import getDeterministicPort from './getDeterministicPort.js'
+import { divide, info } from './logger.js'
+import processTenantLine from './processTenantLine.js'
+import {
+    getContent,
+    getLines,
+    isFile,
+    overrideFile,
+} from './os.js'
+import setupLocalDns from './setupLocalDns.js'
+
+const getDatabaseDomain = originalDomain => `db.search.${originalDomain}`
+
+const createDatabaseComposeFile = params => {
+    const {
+        repo,
+        composeTemplatePath,
+    } = params
+    const composePath = `/tmp/${repo}/search/compose.yaml`
+    const content = getContent(composeTemplatePath)
+    const substituted = content.replace(/\$\{(\w+)\}/g, (_, name) => params[name] || '')
+    overrideFile(composePath, substituted)
+    return composePath
+}
+
+const createSearchDatabaseContainer = params => {
+    const {
+        home,
+        lowercaseRepo,
+    } = params
+    info('Creating database container')
+    const path = createDatabaseComposeFile({
+        ...params,
+        composeTemplatePath: `${home}/core/container/composes/search`
+    })
+    runOnTerminal(`docker compose -p ${lowercaseRepo}-search -f ${path} up -d --remove-orphans`)
+}
+
+export default params => {
+    const {
+        isCiCd,
+        repo,
+        tenantsPath,
+    } = params
+    if (isCiCd) {
+        return
+    }
+    setupLocalDns({
+        ...params,
+        host: `${repo}.local`,
+    })
+    params.databaseSearchPort = getDeterministicPort(repo)
+    const lines = getLines(tenantsPath, 'utf8').filter(Boolean)
+    lines.forEach(line => processTenantLine({
+        ...params,
+        line,
+        getSpecificDomain: getDatabaseDomain,
+    }))
+
+    divide()
+    createSearchDatabaseContainer(params)
+    divide()
+}
