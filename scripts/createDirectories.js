82cc9cc08493c3af5fd080819bbd43480a544b6b
Saeed Nemati <sn.1361@gmail.com>
2026-08-26 13:27:33 +0330

    committed by script

--- scripts/createDirectories.js
+++ scripts/createDirectories.js
@@ -1,5 +1,8 @@
 import getDependencies from './getDependencies.js'
-import { createDirIfNotExists, removeAndRecreateDir } from './os.js'
+import {
+    createDirIfNotExists,
+    removeAndRecreateDir,
+} from './os.js'
 
 export default params => {
     const {
@@ -11,60 +14,99 @@ export default params => {
         repo,
     } = params
     // rmSync(`/tmp/${repo}/${process}`, { recursive: true })
-    const hasSourceDirectory = ['panel', 'site'].includes(processType)
-    const hasRunnableDirectory = ['panel'].includes(processType)
-    const isApi = processType === 'api'
-    if (isApi) {
+    const hasRunnableDirectory = processType === 'panel'
+    const hasSourceDirectory = [
+        'panel',
+        'site',
+    ].includes(processType)
+    const processIsApi = processType === 'api'
+    if (processIsApi) {
         removeAndRecreateDir('/tmp/spl')
     }
-    const tempDirs = [
-        [`/tmp/${repo}`, `${containerHome}/${repo}`],
-        [`/tmp/${repo}/common`, `${containerHome}/${repo}/common`],
-        [`/tmp/${repo}/${process}`, `${containerHome}/${repo}/${process}`],
-        [`/tmp/${repo}/${process}/ast`, `${containerHome}/${repo}/${process}/ast`],
-        [`/tmp/${repo}/${process}/node_modules`, `${containerHome}/${repo}/${process}/node_modules`],
+
+    const directoryEntries = [
+        [
+            `/tmp/${repo}`,
+            `${containerHome}/${repo}`,
+        ],
+        [
+            `/tmp/${repo}/common`,
+            `${containerHome}/${repo}/common`,
+        ],
+        [
+            `/tmp/${repo}/${process}`,
+            `${containerHome}/${repo}/${process}`,
+        ],
+        [
+            `/tmp/${repo}/${process}/ast`,
+            `${containerHome}/${repo}/${process}/ast`,
+        ],
+        [
+            `/tmp/${repo}/${process}/node_modules`,
+            `${containerHome}/${repo}/${process}/node_modules`,
+        ],
         `/var/tmp/${repo}`,
         `/var/tmp/${repo}/${processType}`,
         `/var/tmp/${repo}/${processType}/nodeModules`,
         `/var/tmp/${processType}`,
         `/var/tmp/${processType}/nodeModules`,
-        [`/tmp/spl`, `${containerHome}/spl`],
-        [`${home}/packages`, `${containerHome}/packages`],
-        [`${home}/packages/${processType}`, `${containerHome}/packages/${processType}`],
+        [
+            '/tmp/spl',
+            `${containerHome}/spl`,
+        ],
+        [
+            `${home}/packages`,
+            `${containerHome}/packages`,
+        ],
+        [
+            `${home}/packages/${processType}`,
+            `${containerHome}/packages/${processType}`,
+        ],
     ]
     if (extraDirectories) {
-        tempDirs.push(...extraDirectories)
+        directoryEntries.push(...extraDirectories)
     }
     if (hasSourceDirectory) {
-        tempDirs.push([
+        directoryEntries.push([
             `/tmp/${repo}/${process}/src`,
-            `${home}/${repo}/${process}/src`
+            `${home}/${repo}/${process}/src`,
         ])
     }
     if (hasRunnableDirectory) {
-        tempDirs.push([
+        directoryEntries.push([
             `/tmp/${repo}/${process}/runnable`,
-            `${home}/${repo}/${process}/src/runnable`
+            `${home}/${repo}/${process}/src/runnable`,
         ])
     }
-    for (const tempDir of tempDirs) {
-        if (Array.isArray(tempDir)) {
-            const [left, right] = tempDir
-            createDirIfNotExists(left)
-            params.addVolume(left, right)
+
+    for (const directoryEntry of directoryEntries) {
+        if (Array.isArray(directoryEntry)) {
+            const [
+                sourcePath,
+                targetPath,
+            ] = directoryEntry
+            createDirIfNotExists(sourcePath)
+            params.addVolume(sourcePath, targetPath)
         }
         else {
-            createDirIfNotExists(tempDir)
+            createDirIfNotExists(directoryEntry)
         }
     }
+
     const dependencies = getDependencies(params)
+    const sourceDirectory =
+        hasSourceDirectory
+        ?
+        'src/'
+        :
+        ''
     for (const dependency of dependencies) {
-        tempDirs.push([
-            `/tmp/${repo}/${process}/${hasSourceDirectory ? 'src/' : ''}${dependency}`,
-            `${home}/${repo}/${process}/${hasSourceDirectory ? 'src/' : ''}${dependency}`
+        directoryEntries.push([
+            `/tmp/${repo}/${process}/${sourceDirectory}${dependency}`,
+            `${home}/${repo}/${process}/${sourceDirectory}${dependency}`,
         ])
-        if (isApi) {
-            tempDirs.push([
+        if (processIsApi) {
+            directoryEntries.push([
                 `/tmp/${repo}/${process}/${dependency}`,
                 `${home}/${repo}/${process}/node_modules/${dependency}`,
             ])
