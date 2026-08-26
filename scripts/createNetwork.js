cffeecfed80169555781b0ab80102d532c6ae836
Saeed Nemati <sn.1361@gmail.com>
2026-08-26 12:41:38 +0330

    committed by script

--- scripts/createNetwork.js
+++ scripts/createNetwork.js
@@ -2,12 +2,10 @@ import { info } from '../scripts/logger.js'
 import { runOnTerminal } from './terminal.js'
 
 export default params => {
-    const {
-        repo,
-    } = params
+    const { repo } = params
     const networkName = `${repo}Network`
-    const output = runOnTerminal('docker network ls --format {{.Name}}').split('\n')
-    if (!output.includes(networkName)) {
+    const networkNames = runOnTerminal('docker network ls --format {{.Name}}').split('\n')
+    if (!networkNames.includes(networkName)) {
         info(`Creating the network ${networkName}`)
         runOnTerminal(`docker network create --driver bridge ${networkName}`, {
             show: false,
